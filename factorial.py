a41a8c9ec0ba9709e624c4be65af4c971a5e162d
SachalaH <59394760+SachalaH@users.noreply.github.com>
2021-09-20 17:09:35 +0530

    Factorial of numbers 0 to 10.

--- factorial.py
+++ factorial.py
@@ -14,5 +14,6 @@ def factorial(n):
 
 
 # Driver Code
-num = 5
-print("Factorial of", num, "is", factorial(num))
+nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for num in nums:
+    print("Factorial of", num, "is", factorial(num))
